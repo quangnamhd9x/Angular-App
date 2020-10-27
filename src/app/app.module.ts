6f7466609247d426a0660a5d36eb7ffd4d54ee06
QuangNam <quangnamhd9x@gmail.com>
2020-10-27 10:32:36 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { MenuRightComponent } from './menu-right/menu-right.component';
+import { MenuLeftComponent } from './menu-left/menu-left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    HeaderComponent,
+    BodyComponent,
+    MenuRightComponent,
+    MenuLeftComponent
   ],
   imports: [
     BrowserModule,
